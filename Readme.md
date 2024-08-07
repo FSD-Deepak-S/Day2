4e44cf593ef2f53f0dcef2cf67ccf0fa9af7e86a
FSD-Deepak-S <devold94@gmail.com>
2024-08-07 21:05:09 +0530

    Java script class Day 2

---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is Javascript Day2 in the road map. The contents are listed below
+    # variables & Datatypes
+    # Alerts, prompts, Confirm
+    # Operators
---
+++ script.js
@@ -0,0 +1,49 @@
+console.log('welcome to the day 2 Javascript ')
+
+// var s  = 10
+// s = 20 //reassign
+// var s = 30 //redeclare
+//  console.log(s)
+
+// let a = 30;
+// a = 40
+// console.log(a)
+
+// const b = 10
+// b = 2
+// console.log(b)
+
+// alert('Hello World!')
+
+// let name = prompt("Enter your name")
+// console.log(name);
+
+// let xx = confirm("confirm to forward next")
+// console.log(xx)
+
+// let a 
+// console.log(typeof(a));
+
+// let n = null;
+// console.log(n);
+// console.log(typeof(n));
+
+/*
+let person  = {
+    fname:"Deepak",
+    lname:"S",
+    gender:"Male",
+    age:32,
+    mobile:9578677143,
+    skills:['developer','HTML','CSS','Javascript'],
+    family:{
+        father_name:"Sankar",
+        mother_name:"Maragatham",
+        wife_name:"Priya",
+        Daughter_name:"Thariga"
+    }
+}
+
+console.log(person);
+console.log(typeof(person));
+*/
